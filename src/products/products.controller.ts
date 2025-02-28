04f81772e54b7c775ca55dc49cbaad7c2e02633d
Nelly <nelly.babillon@gmail.com>
2025-02-28 09:37:22 +0100

    Add description to response

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
@@ -13,11 +16,17 @@ export class ProductsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Récupérer tous les produits' })
+  @ApiResponse({ status: 200, description: 'Tous les produits trouvés', type: Product })
+  @ApiResponse({ status: 404, description: 'Aucun produit trouvé' })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Récupérer un produit par ID' })
+  @ApiResponse({ status: 200, description: 'Produit trouvé', type: Product })
+  @ApiResponse({ status: 404, description: 'Produit non trouvé' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
